2bcfb2fa2be2af445ccf2fe0fbfd1d3b877132cf
hagna <natester@gmail.com>
2014-05-18 00:04:02 +0000

    some tests seem to pass in simple cases

--- cpt.go
+++ cpt.go
@@ -24,11 +24,33 @@ func (t *tree) print(n *node) {
 }
 
 func (t *tree) insert(root *node, k, v string) {
-	n, m := t.lookup(root, v)
+	n, m := t.lookup(root, k)
 	fmt.Println("lookup found", n, m)
 	if n == nil {
 		newnode := &node{v, k, nil}
 		root.children = append(root.children, newnode)
+		return
+	}
+	if m == n.edgename {
+		// simple case just add the rest
+		fmt.Println("simple case add a node")
+		nk := k[len(m):]
+		if len(nk) > 0 {
+			newnode := &node{v, nk, nil}
+			n.children = append(n.children, newnode)
+		} else {
+			fmt.Println("node exists already")
+		}
+	} else {
+		fmt.Println("split the node case")
+		mp := matchprefix(m, n.edgename)
+		nk := k[len(mp):]
+		newnodeA := &node{v, nk, nil}
+		rnk := n.edgename[len(mp):]
+		newnodeB := &node{n.value, rnk, n.children}
+		n.edgename = mp
+		n.children = append(n.children, newnodeA)
+		n.children = append(n.children, newnodeB)
 	}
 
 }
@@ -53,16 +75,19 @@ func matchprefix(a, b string) string {
 }
 
 func (t *tree) lookup(n *node, s string) (nres *node, match string) {
+	fmt.Println("lookup", *n, s)
 	if s == "" {
 		return n, ""
 	}
 	for _, c := range n.children {
+		fmt.Println("checking child", *c)
 		match = matchprefix(c.edgename, s)
+		fmt.Printf("got match ('%s', '%s') -> '%s'\n", c.edgename, s, match)
 		if match == "" {
 			continue
 		} else {
 			var m string
-			nres, m = t.lookup(c, s[:len(m)])
+			nres, m = t.lookup(c, s[len(m):])
 			match += m
 			// for a partial match
 			if nres == nil {
--- cpt_test.go
+++ cpt_test.go
@@ -2,8 +2,30 @@ package typefaster
 
 import "testing"
 
+func TestInsertDup(t *testing.T) {
+	tree := tree{&node{"root", "", nil}}
+	tree.insert(tree.root, "T.AH.P", "top")
+	tree.insert(tree.root, "T.AH.P", "top")
+	if len(tree.root.children) > 1 {
+		t.Fatalf("should only be one child")
+	}
+}
+
 func TestInsert(t *testing.T) {
 	tree := tree{&node{"root", "", nil}}
-	tree.insert(tree.root, "top", "T.AH.P")
+	tree.insert(tree.root, "T.AH.P", "top")
+	tree.insert(tree.root, "T.AH.P.S", "tops")
 	tree.print(tree.root)
+	if len(tree.root.children) == 2 {
+		t.Fatalf("should only two children")
+	}
+}
+
+func TestInsertSplit(t *testing.T) {
+	tree := tree{&node{"root", "", nil}}
+	tree.insert(tree.root, "T.AH.P", "top")
+	tree.insert(tree.root, "T.AH.T", "tot")
+	if len(tree.root.children) == 2 {
+		t.Fatalf("should only two children")
+	}
 }
